dfa980efbf32de658901169665724fc78dfff8d4
hjjjong0811 <hjjjong0811@gmail.com>
2017-04-16 23:39:26 +0900

    Loading, Login Activity 생성
    기능은 없다.....ㅠ 테스트로 Loading에 시간지나면 L:ogin으로 옮겨가게 해놨음

--- HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/LoadingActivity.java
+++ HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/LoadingActivity.java
@@ -1,7 +1,12 @@
 package comjungwon7769heartbeat.github.heartbeat;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -12,14 +17,41 @@ public class LoadingActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_loading);
+
+		Data_Check = false;
+		Login_Check = false;
+
+		Data_Check = App_Data_Check();
+
+		if(Data_Check == true) {
+			Login_Check = Login_Usable_Check();
+		}
+
+		if(Login_Check == true) {
+			//친구목록화면으로
+		}
+
+		new Timer().schedule(new TimerTask() {
+			public void run() {
+				test();
+			}
+		}, 1000);
+
+
 	}
 
-	private  boolean App_Data_Check(){
+	protected  void test(){
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivity(intent);
+		finish();
+	}
+
+	private boolean App_Data_Check() {
 
 		return false;
 	}
 
-	private boolean Login_Usable_Check(){
+	private boolean Login_Usable_Check() {
 
 		return false;
 	}
--- HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/LoginActivity.java
+++ HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/LoginActivity.java
@@ -2,12 +2,36 @@ package comjungwon7769heartbeat.github.heartbeat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+	EditText txtId, txtPwd;
+	Button btnLogin, btnJoin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		txtId = (EditText) findViewById(R.id.txtID);
+		txtPwd = (EditText) findViewById(R.id.txtPWD);
+		btnLogin = (Button) findViewById(R.id.btnLogin);
+		btnJoin = (Button) findViewById(R.id.btnJoin);
+
+		btnLogin.setOnClickListener(btnLogin_click);
+
 	}
+
+	Button.OnClickListener btnLogin_click = new View.OnClickListener() {
+		public void onClick(View v) {
+			//이곳에 버튼 클릭시 일어날 일을 적습니다.
+			TextView textV = (TextView)findViewById(R.id.txtLogo);
+			textV.setText("HOHO");
+		}
+	};
+
 }
