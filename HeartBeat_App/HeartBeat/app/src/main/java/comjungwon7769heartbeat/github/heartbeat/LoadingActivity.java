dd5c6a909737226ad913dc23c4743b7cdcc2dc25
hjjjong0811 <hjjjong0811@gmail.com>
2017-04-30 23:23:38 +0900

    Revert "Revert "FriendDTO, FriendListAct.., JoinAct.. 생성및수정""
    This reverts commit cc29904c5e919b8f3b25cb5e5f16576a20cb6477.

--- HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/LoadingActivity.java
+++ HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/LoadingActivity.java
@@ -41,7 +41,7 @@ public class LoadingActivity extends AppCompatActivity {
 	}
 
 	protected  void test(){
-		Intent intent = new Intent(this, LoginActivity.class);
+		Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
 		startActivity(intent);
 		finish();
 	}
