c1c83b65174c12424d9374bdf14374cbe135fcc2
hjjjong0811 <hjjjong0811@gmail.com>
2017-04-30 20:31:21 +0900

    Join, Loading, Login Act들 버튼리스너 연결, 스타일지정
    스타일만 지정했는데 왜 자꾸 오류?

--- HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/JoinActivity.java
+++ HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/JoinActivity.java
@@ -2,21 +2,54 @@ package comjungwon7769heartbeat.github.heartbeat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class JoinActivity extends Activity {
+
+	private EditText txtID, txtPWD, txtNick;
+	private Button btnJoin, btnUsable;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_join);
+
+		txtID = (EditText)findViewById(R.id.join_txtID);
+		txtPWD = (EditText)findViewById(R.id.join_txtPWD);
+		txtNick = (EditText)findViewById(R.id.join_txtNick);
+		btnUsable = (Button) findViewById(R.id.join_btnUsable);
+		btnJoin = (Button) findViewById(R.id.join_btnJoin);
+
+		//중복검사 버튼 리스너 지정(ID_Usable_Check)
+		btnUsable.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v) {
+                    ID_Usable_Check(txtID.getText().toString());
+                }
+            }
+		);
+		//회원가입 버튼 리스너 지정(Join)
+		btnJoin.setOnClickListener(new View.OnClickListener(){
+                 public void onClick(View v) {
+                     Join(txtID.getText().toString(), txtPWD.getText().toString(), txtNick.getText().toString());
+                 }
+             }
+		);
+
 	}
 
 	private  boolean ID_Usable_Check(String id){
+		//ServerComu class Create
+		//Request ID Exist
 
 		return false;
 	} //id_usable_check()
 
 	private void Join(String id, String pwd, String Nick){
+		//ServerComu Class Create
+		//Request Join
 
+		finish();
 	} //join()
 }
--- HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/LoginActivity.java
+++ HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/LoginActivity.java
@@ -1,5 +1,6 @@
 package comjungwon7769heartbeat.github.heartbeat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,29 +10,53 @@ import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-	EditText txtId, txtPwd;
-	Button btnLogin, btnJoin;
+	private EditText txtId, txtPwd;
+	private Button btnLogin, btnJoin;
+	private boolean check;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 
-		txtId = (EditText) findViewById(R.id.txtID);
-		txtPwd = (EditText) findViewById(R.id.txtPWD);
-		btnLogin = (Button) findViewById(R.id.btnLogin);
-		btnJoin = (Button) findViewById(R.id.btnJoin);
+		txtId = (EditText) findViewById(R.id.login_txtID);
+		txtPwd = (EditText) findViewById(R.id.login_txtPWD);
+		btnLogin = (Button) findViewById(R.id.login_btnLogin);
+		btnJoin = (Button) findViewById(R.id.login_btnJoin);
 
-		btnLogin.setOnClickListener(btnLogin_click);
 
-	}
+		btnLogin.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Login_Usable_Check(txtId.getText().toString(), txtPwd.getText().toString());
+			}
+		});
+		btnJoin.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Join_Button();
+			}
+		});
 
+	} //onCreate()
+
+	/*
 	Button.OnClickListener btnLogin_click = new View.OnClickListener() {
 		public void onClick(View v) {
 			//이곳에 버튼 클릭시 일어날 일을 적습니다.
-			TextView textV = (TextView)findViewById(R.id.txtLogo);
+			TextView textV = (TextView)findViewById(R.id.login_txtLogo);
 			textV.setText("HOHO");
 		}
 	};
+	*/
 
+	private boolean Login_Usable_Check(String id, String pwd) {
+		TextView textV = (TextView) findViewById(R.id.login_txtLogo);
+		textV.setText(id + pwd);
+
+		return false;
+	} //loginUsableCheck()
+
+	private void Join_Button() {
+		Intent intent = new Intent(this, JoinActivity.class);
+		startActivity(intent);
+	} //join_button()
 }
