bf242dcd652a85614f7854f05a04d511e3164014
hjjjong0811 <hjjjong0811@gmail.com>
2017-05-03 00:15:02 +0900

    FriendList와 Enum(Emotion)대폭수정
    상수용클래스 Constants 추가
    Emotion Enum 수정
    FriendDTO 수정(Mode관련 get set 추가)
    Login, Join String 길이 제한 수정
    FriendList 색, 기분 표시 추가, 레이아웃수정
    
    기분이미지 추가(색 변경 있음 별표친것들)
    * 0 smile 59BE7F
    * 1 laugh FFFF00
    * 3 annoy FF9900
    
    * 6 love FF93B2
    * 7 wow 7393D5
    * 8 overeat D9CA74
    
    2 sad FFBB00
    4 angry FF0000
    5 wink 8041D9
    9 sleep FFFFFF

---
+++ HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/Constants.java
@@ -0,0 +1,28 @@
+package comjungwon7769heartbeat.github.heartbeat;
+
+/**
+ * Created by AH on 2017-05-02.
+ */
+public class Constants {
+	public static final int maxString = 32;
+	public static final int minString = 4;
+
+	public static enum Emotion{
+		smile(0, "59BE7F"), laugh(1, "FFFF00"), sad(2, "FFBB00"), annoy(3, "FF9900"), angry(4, "FF0000"),
+		wink(5, "8041D9"), love(6, "FF93B2"), wow(7, "7393D5"), overeat(8, "D9CA74"), sleep(9, "FFFFFF");
+
+		private int mode;
+		private String color;
+
+		Emotion(int value, String colorValue){
+			this.mode = value;
+			this.color = colorValue;
+		}
+		public int getMode(){return mode;}
+		public void setMode(int value){this.mode = value;}
+
+		public String getColor(){return color;}
+		public void setMode(String colorValue){this.color = colorValue;}
+	} //enum Emotion
+
+}
