ef46afde56f38d18ff516a4f97d82caf4618c216
hjjjong0811 <hjjjong0811@gmail.com>
2017-05-04 14:44:58 +0900

    POPUP 관련 진행, MyDetail 수정
    Constants.java 팝업액티비티 종류별 코드와 intent 이용해 보내고 받을 데이터 이름 정함
    MyDetailAct 버튼리스너, 메소드 연결 / 팝업 액티비티 띄우기 추가(색선택) / 프리퍼런스데이터 로드(내정보)
    popupActivity 색상선택 팝업 타입지정 및 액티비티간 데이터 전달 (색상선택기용 레이아웃, 코드는 아직 구현X)

--- HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/PopupActivity.java
+++ HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/PopupActivity.java
@@ -3,6 +3,7 @@ package comjungwon7769heartbeat.github.heartbeat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -32,6 +33,24 @@ public class PopupActivity extends AppCompatActivity {
 					public void onClick(View v) {
 						Intent intent = new Intent();
 						intent.putExtra("result", "팝업 그만~~");
+						setResult(RESULT_OK, intent);
+
+						finish();
+					}
+				});
+
+				break;
+
+			case Constants.popup_pickColor:
+				setContentView(R.layout.activity_popup_ok);
+
+				btnOK = (Button)findViewById(R.id.popup_ok_btnOK);
+				btnOK.setOnClickListener(new Button.OnClickListener(){
+					@Override
+					public void onClick(View v) {
+						Intent intent = new Intent();
+						intent.putExtra("Popup", Constants.popup_pickColor);
+						intent.putExtra("selectedColor", "66FF22");
 						setResult(RESULT_OK, intent);
 
 						finish();
