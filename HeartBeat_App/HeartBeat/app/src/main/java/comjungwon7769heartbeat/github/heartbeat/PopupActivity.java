b3df8227ff58c880aa6e5868124032a824bfd200
hjjjong0811 <hjjjong0811@gmail.com>
2017-05-06 00:31:45 +0900

    ColorPickerView class 추가, 스타일수정, popup수정
    ColorPickerView.java 클래스추가 : 색상선택기용. -> 현재 색상 쉐이더로 표시하기, 터치했을때 터치한 각도
    구하기까지햇음.
    ->안드로이드 뷰상속이랑 쉐이더랑 삼각함수관련 이해부족으로 좀더 걸릴것같음;;(**공부!)
    popupAct LED색설정 클릭시 색상선택창 나오게함
    activity_popup_pickcolor 레이아웃 추가
    기타 스타일 수정

--- HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/PopupActivity.java
+++ HeartBeat_App/HeartBeat/app/src/main/java/comjungwon7769heartbeat/github/heartbeat/PopupActivity.java
@@ -20,50 +20,56 @@ public class PopupActivity extends AppCompatActivity {
 		int popupMode = intent.getIntExtra("Popup", 0);
 
 		//Popup 종류별로 레이아웃 및 초기화
-		switch(popupMode){
-			case Constants.popup_ok:
-				setContentView(R.layout.activity_popup_ok);
+		//popup_ok
+		if(popupMode == Constants.popup_ok){
+			setContentView(R.layout.activity_popup_ok);
+			setTitle(R.string.popup_ok_label);
 
-				TextView message = (TextView) findViewById(R.id.popup_ok_txt);
-				message.setText(intent.getStringExtra("Message"));
+			//팝업을 호출한 액티비티로부터 데이터를 불러와 셋팅
+			TextView message = (TextView) findViewById(R.id.popup_ok_txt);
+			message.setText(intent.getStringExtra("Message"));
 
-				Button btnOK = (Button)findViewById(R.id.popup_ok_btnOK);
-				btnOK.setOnClickListener(new Button.OnClickListener(){
-					@Override
-					public void onClick(View v) {
-						Intent intent = new Intent();
-						intent.putExtra("result", "팝업 그만~~");
-						setResult(RESULT_OK, intent);
+			//팝업의 OK 버튼의 리스너 지정
+			Button btnOK = (Button)findViewById(R.id.popup_ok_btnOK);
+			btnOK.setOnClickListener(new Button.OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					//확인버튼 클릭시 액티비티 종료
+					Intent intent = new Intent();
+					intent.putExtra("Popup", Constants.popup_ok);
+					setResult(RESULT_OK, intent);
 
-						finish();
-					}
-				});
-
-				break;
-
-			case Constants.popup_pickColor:
-				setContentView(R.layout.activity_popup_ok);
-
-				btnOK = (Button)findViewById(R.id.popup_ok_btnOK);
-				btnOK.setOnClickListener(new Button.OnClickListener(){
-					@Override
-					public void onClick(View v) {
-						Intent intent = new Intent();
-						intent.putExtra("Popup", Constants.popup_pickColor);
-						intent.putExtra("selectedColor", "66FF22");
-						setResult(RESULT_OK, intent);
-
-						finish();
-					}
-				});
-
-				break;
-
-			default:
-				break;
+					finish();
+				}
+			});
 		}
+		//popup_pickColor
+		else if(popupMode == Constants.popup_pickColor){
+			setContentView(R.layout.activity_popup_pickcolor);
+			setTitle(R.string.popup_pickColor_label);
+
+			//ColorPickerView colorPickerView = (ColorPickerView)findViewById(R.id.popup_pickColor_colorPickerView);
+
+			Button btnOK = (Button)findViewById(R.id.popup_pickColor_ok);
+			btnOK.setOnClickListener(new Button.OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					//확인버튼 클릭시 선택한 색상 전달후 액티비티 종료
+					Intent intent = new Intent();
+					intent.putExtra("Popup", Constants.popup_pickColor);
+					intent.putExtra("selectedColor", "66FF22");
+					setResult(RESULT_OK, intent);
+
+					finish();
+				}
+			});
+		}
+
+
+
 	} // onCreate
 
+	/*
 	@Override
 	public boolean onTouchEvent(MotionEvent event){
 		if(event.getAction()==MotionEvent.ACTION_OUTSIDE){
@@ -71,6 +77,7 @@ public class PopupActivity extends AppCompatActivity {
 		}
 		return true;
 	}
+	*/
 
 
 
